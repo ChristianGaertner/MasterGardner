1da180bb33cb84a296809a434a203d4d2dd95322
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-04-10 17:55:28 +0200

    Random Level Basis

---
+++ src/mastergardner/level/RandomLevel.java
@@ -0,0 +1,33 @@
+/*
+ * Random Gen for the level
+ *
+ */
+package mastergardner.level;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Christian
+ */
+public class RandomLevel extends Level{
+    
+    private final Random random;
+    
+    public RandomLevel(int width, int height) {
+        super(width, height);
+        this.random = new Random();
+    }
+    
+    /**
+     * This will generate the random level, obviously
+     */
+    @Override
+    protected void generateLevel() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                tiles[x + y * width] = random.nextInt(4);
+            }
+        }
+    }
+}
